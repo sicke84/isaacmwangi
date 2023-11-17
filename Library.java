93c0a03e1479b5954f0e5764ccd90b349b018155
sicke84 <147137636+sicke84@users.noreply.github.com>
2023-11-17 13:38:27 +0300

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Library {
+
+    public static void main(String[]args){
+        
+
+        System.out.println("title:Hope");
+        System.out.println("yearofpublishing:2006");
+        System.out.println("author:Isaac");
+        System.out.println("ISBN:2200");
+        
+    }
+}
